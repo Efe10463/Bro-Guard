f79df8d0b2906f3c46f25c0b508538cd3ce18084
Efe10463 <71014843+Efe10463@users.noreply.github.com>
2021-01-27 18:30:05 +0300

    Add files via upload

---
+++ komutlar/davet.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js"),
+  db = require("quick.db");
+
+module.exports.run = async (client, message, args) => {
+  const embed = new Discord.RichEmbed()
+    .setDescription(
+      `[Tıkla!](hhttps://discordapp.com/oauth2/authorize?client_id=706484715481464833&scope=bot&permissions=805314622=${client.user.id}&scope=bot&permissions=8)`
+    )
+
+    .setColor("BLACK")
+    .setFooter(client.user.username, client.user.avatarURL);
+  message.channel.send(embed);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "davet",
+  description: "davet",
+  usage: "davet"
+};
